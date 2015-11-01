bd7180310a951a381da81f4abb8b8cb17604ce85
Adi Rotem <Adi.Rotem@mail.huji.ac.il>
2015-11-01 17:19:05 +0200

    added lenged and bar plot options

--- colorPlot.m
+++ colorPlot.m
@@ -1,7 +1,22 @@
-function colorPlot(x,y)
+function colorPlot(x,y,varargin)
 % every row in x is an x axis to plot
 % every rew in y is a y axis to plot
 % this will plot all [x, y]'s in different colors
+   
+% optional: 'labels': a cell array of strings to be used as legend
+% optional: 'plotBar': plot as bar
+
+p = inputParser();
+
+addOptional(p, 'addLegend', []);
+addOptional(p, 'plotBar', false);
+
+parse(p, varargin{:});
+Results = p.Results;
+
+addLegend = Results.addLegend;
+plotBar = Results.plotBar;
+
     [numOfPlots,~] = size(x);
     
   
@@ -10,7 +25,16 @@ function colorPlot(x,y)
    figure; hold on;
    
    for i = 1:numOfPlots
-       plot(x(i,:),y(i,:),'color',cmap(i,:));
+       if plotBar
+            increment = x(i,2)-x(i,1);
+            bar(x(i,:)+increment/2,y(i,:),increment,'EdgeColor',cmap(i,:))
+       else
+            plot(x(i,:),y(i,:),'color',cmap(i,:));
+       end
+   end
+   
+   if ~isempty(addLegend)
+        legend(addLegend)
    end
    
 end
